79de99d9bd20815e5dcc8faa231df8a49aae8563
nasehqusyairy <nasyikhqusyairy@gmail.com>
2021-11-12 01:58:59 +0700

    gambar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $(".navbar .nav-link,.navbar-brand").click(function (e) {
+  $(".navbar .nav-link,.navbar-brand,.jumbotron a").click(function (e) {
     let href = $(this).attr("href");
     $("html").animate({
       scrollTop: $(href).offset().top - 56
